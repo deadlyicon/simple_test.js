ebda760523a6d01b58c0864395d790786706b28f
Jared Grippe <jared@deadlyicon.com>
2010-02-04 23:45:25 -0800

    added browser and non-browser examples

--- exmaple.js
+++ exmaple.js
@@ -1,8 +1,14 @@
+// Using V8 or Rhino
+load('simple_test.js');
+
 new SimpleTestSuite(function(test){
 
   test('the truth', function(){
     return true;
   });
 
-  console.dir(this.tests);
+  print(this.tests.passed+' out of '+this.tests.failed+' tests passed');
+  for (var i=0; i < this.tests.length; i++) {
+    print((this.tests[i][0] ? 'PASS' : 'FAIL')+': '+this.tests[i][1]);
+  };
 });
